ffbaa91bd681a37f5e95fc9ade5e22834ab08e6d
Yahya <yo03451122@gmail.com>
2025-08-06 21:05:40 -0500

    Add helloWorld.js

---
+++ helloWorld.js
@@ -0,0 +1,21 @@
+console.log("Hello World")
+
+function addtion(x,y) {
+
+     const  z =  x +  y;
+     return z;
+}
+function subtraction(x,y){
+    const  z =  x -  y;
+     return z;
+}
+
+function multiply(x,y){
+    const  z =  x * y;
+     return z;
+}
+
+function division(x,y){
+    const  z =  x /  y;
+     return z;
+}
